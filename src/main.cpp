0feff87c63946c7c5468ef0d1d9c995a45d52627
NukeBird <nukebird.dev@gmail.com>
2022-02-03 22:42:49 +0300

    Basic magnum + glfw setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,44 @@
-int main()
+#include <openvr/openvr.h>
+#include <Magnum/Magnum.h>
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Platform/GLContext.h>
+#include <Magnum/Shaders/VertexColor.h>
+#include <GLFW/glfw3.h>
+#include <Magnum/GlmIntegration/Integration.h>
+#include <Magnum/GlmIntegration/GtcIntegration.h>
+#include <Magnum/GlmIntegration/GtxIntegration.h>
+
+#include <vector>
+
+int main(int argc, char** argv) 
 {
-	return 0;
+    if (!glfwInit()) return -1;
+
+    glm::ivec2 window_size{ 800, 600 };
+
+    GLFWwindow* const window = glfwCreateWindow(window_size.x, window_size.y, "GLFW", nullptr, nullptr);
+
+    if (!window) 
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    {
+        Magnum::Platform::GLContext ctx{ argc, argv };
+
+        while (!glfwWindowShouldClose(window)) 
+        {
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        glfwTerminate();
+    }
+
+    return 0;
 }
